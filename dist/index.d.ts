3c06548c01639e16c403d1df4eca22018b7f0199
wangshaoxing <wshxbqq@gmail.com>
2017-03-21 13:20:49 +0800

    add .

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import DirectoryInfo from "./lib/DirectoryInfo";
-import FileInfo from "./lib/FileInfo";
-declare var _default: {
-    DirectoryInfo: typeof DirectoryInfo;
-    FileInfo: typeof FileInfo;
-};
-export default _default;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var DirectoryInfo_1 = require("./lib/DirectoryInfo");
-var FileInfo_1 = require("./lib/FileInfo");
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    DirectoryInfo: DirectoryInfo_1.default,
-    FileInfo: FileInfo_1.default
-};
-//# sourceMappingURL=index.js.map
